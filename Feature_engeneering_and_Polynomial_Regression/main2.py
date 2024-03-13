1ebf2a508b986d846134f52ef1df4b462009537c
Mareczek1311 <mkretkowski131@gmail.com>
2024-03-13 17:42:21 +0100

    feature engeneering

---
+++ Feature_engeneering_and_Polynomial_Regression/main2.py
@@ -0,0 +1,79 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math, copy
+
+# create target data
+x = np.arange(0, 20, 1)
+y = x**2
+
+# engineer features .
+X = np.c_[x**2]   #<-- added engineered feature
+
+def compute_cost(X, y, w, b): 
+    m = X.shape[0]
+    cost = 0.0
+    for i in range(m):                                
+        f_wb_i = np.dot(X[i],w) + b       
+        cost = cost + (f_wb_i - y[i])**2              
+    cost = cost/(2*m)                                 
+    return(np.squeeze(cost)) 
+
+def compute_gradient_matrix(X, y, w, b): 
+    m,n = X.shape
+    f_wb = X @ w + b              
+    e   = f_wb - y                
+    dj_dw  = (1/m) * (X.T @ e)    
+    dj_db  = (1/m) * np.sum(e)    
+        
+    return dj_db,dj_dw
+
+def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters): 
+    # number of training examples
+    m = len(X)
+    
+    # An array to store values at each iteration primarily for graphing later
+    hist={}
+    hist["cost"] = []; hist["params"] = []; hist["grads"]=[]; hist["iter"]=[];
+    
+    w = copy.deepcopy(w_in)  #avoid modifying global w within function
+    b = b_in
+    save_interval = np.ceil(num_iters/10000) # prevent resource exhaustion for long runs
+    
+    for i in range(num_iters):
+
+        # Calculate the gradient and update the parameters
+        dj_db,dj_dw = gradient_function(X, y, w, b)   
+
+        # Update Parameters using w, b, alpha and gradient
+        w = w - alpha * dj_dw               
+        b = b - alpha * dj_db               
+      
+        # Save cost J,w,b at each save interval for graphing
+        if i == 0 or i % save_interval == 0:     
+            hist["cost"].append(cost_function(X, y, w, b))
+            hist["params"].append([w,b])
+            hist["grads"].append([dj_dw,dj_db])
+            hist["iter"].append(i)
+
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters/10) == 0:
+            #print(f"Iteration {i:4d}: Cost {cost_function(X, y, w, b):8.2f}   ")
+            cst = cost_function(X, y, w, b)
+            print(f"Iteration {i:9d}, Cost: {cst:0.5e}")
+    return w, b, hist #return w,b and history for graphing
+
+def run_gradient_descent_feng(X,y,iterations=1000, alpha = 1e-6):
+    m,n = X.shape
+    # initialize parameters
+    initial_w = np.zeros(n)
+    initial_b = 0
+    # run gradient descent
+    w_out, b_out, hist_out = gradient_descent(X ,y, initial_w, initial_b,
+                                               compute_cost, compute_gradient_matrix, alpha, iterations)
+    print(f"w,b found by gradient descent: w: {w_out}, b: {b_out:0.4f}")
+    
+    return(w_out, b_out)
+model_w,model_b = run_gradient_descent_feng(X, y, iterations=10000, alpha=1e-7)
+
+plt.scatter(x, y, marker='x', c='r', label="Actual Value"); plt.title("x, x**2, x**3 features")
+plt.plot(x, X@model_w + model_b, label="Predicted Value"); plt.xlabel("x"); plt.ylabel("y"); plt.legend(); plt.show()
